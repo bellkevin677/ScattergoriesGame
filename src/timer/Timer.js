169b8fc9c7e72c198ebefc5fcdab4b11c3203cb1
bellkevin677 <bellkevin677@gmail.com>
2021-04-26 08:28:42 -0500

    Completed

--- src/timer/Timer.js
+++ src/timer/Timer.js
@@ -1,16 +1,42 @@
+import { useEffect, useState } from 'react';
 import './Timer.css'
 
-const Timer = () => {
-    return(
-        <div className='Timer'>
-            <div className='Timer-Top'>
-                <p>TIMER</p>
-                <button className='Timer-Top-Btn'>PLAY</button>
-            </div>
-            <div className='Timer-Display'>
-                <h1 className='Timer-Display-H1'>60</h1>
-            </div>
-        </div>
-    );
+const Timer = ({setHide}) => {
+  const startingTime = 60;
+  const [time, setTime] = useState(startingTime);
+
+  const playGame = () => {
+    setTime(time - 1);
+    setHide(false);
+  }
+
+  useEffect(() => {
+    if(time !== startingTime){
+      setTimeout(() => {
+        if(time > 0){
+            setTime(time - 1);
+        } else {
+          setTime (startingTime);
+          setHide(true);
+          setTimeout(() => {
+              alert('TIME IS UP!!!');
+              setHide(false);
+          }, 100);
+        }
+      }, 1000);
+    }
+  }, [time]);
+
+  return(
+    <div className='Timer'>
+      <div className='Timer-Top'>
+        <p>TIMER</p>
+        <button onClick={playGame} className='Timer-Top-Btn'>PLAY</button>
+      </div>
+      <div className='Timer-Display'>
+        <h1 className='Timer-Display-H1'>{time}</h1>
+      </div>
+    </div>
+  );
 }
 export default Timer;
